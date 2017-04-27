f5328d382e4cb5732c8594b4c378f0591608c1d8
weiwosuoai <weiwosuoai1991@gmail.com>
2017-04-27 10:28:24 +0800

    reformat the sql file called "database.sql"

--- sql/database.sql
+++ sql/database.sql
@@ -17,37 +17,27 @@
 DROP TABLE IF EXISTS t_user;
 CREATE TABLE t_user (
   id          INT(10)      NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(100) NOT NULL
-  COMMENT '用户名',
-  password    VARCHAR(100) NOT NULL
-  COMMENT '密码',
+  name        VARCHAR(100) NOT NULL COMMENT '用户名',
+  password    VARCHAR(100) NOT NULL COMMENT '密码',
   create_time TIMESTAMP    NOT NULL DEFAULT NOW(),
   update_time TIMESTAMP    NOT NULL DEFAULT NOW(),
   remark      VARCHAR(500) COMMENT '备注',
   PRIMARY KEY (id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 -- 文章表 Create on 2017-03-14 by Allen.
 DROP TABLE IF EXISTS t_article;
 CREATE TABLE t_article (
   id          INT          NOT NULL AUTO_INCREMENT,
-  title       VARCHAR(500) NOT NULL
-  COMMENT '文章标题',
-  category    INT          NOT NULL
-  COMMENT '所属分类',
-  file_name   VARCHAR(500) NOT NULL
-  COMMENT '文章原始文件名称',
-  file_path   VARCHAR(500) NOT NULL
-  COMMENT '文章存储路径',
+  title       VARCHAR(500) NOT NULL COMMENT '文章标题',
+  category    INT          NOT NULL COMMENT '所属分类',
+  file_name   VARCHAR(500) NOT NULL COMMENT '文章原始文件名称',
+  file_path   VARCHAR(500) NOT NULL COMMENT '文章存储路径',
   create_time TIMESTAMP    NOT NULL DEFAULT NOW(),
   update_time TIMESTAMP    NOT NULL,
   remark      VARCHAR(500),
   PRIMARY KEY (id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 -- 文章表中添加文章创建者字段 Create on 2017-03-18 by Allen.
 ALTER TABLE t_article ADD create_user_id INT NOT NULL;
@@ -59,31 +49,24 @@ ALTER TABLE t_article ADD be_viewd_num INT NOT NULL DEFAULT 1;
 DROP TABLE IF EXISTS t_category;
 CREATE TABLE t_category (
   id             INT          NOT NULL AUTO_INCREMENT,
-  name           VARCHAR(500) NOT NULL
-  COMMENT '分类名称',
+  name           VARCHAR(500) NOT NULL COMMENT '分类名称',
   create_time    TIMESTAMP    NOT NULL DEFAULT NOW(),
   update_time    TIMESTAMP    NOT NULL DEFAULT NOW(),
-  create_user_id INT          NOT NULL
-  COMMENT '创建者id',
+  create_user_id INT          NOT NULL COMMENT '创建者id',
   remark         VARCHAR(500),
   PRIMARY KEY (id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 -- 标签表 Create on 2017-03-30 by Allen.
 DROP TABLE IF EXISTS t_tag;
 CREATE TABLE t_tag (
   id          INT          NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(200) NOT NULL
-  COMMENT '分类名称',
+  name        VARCHAR(200) NOT NULL COMMENT '分类名称',
   create_time TIMESTAMP    NOT NULL DEFAULT NOW(),
   update_time TIMESTAMP    NOT NULL DEFAULT NOW(),
   remark      VARCHAR(500),
   PRIMARY KEY (id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 -- 添加文章分类  Create on 2017-03-30 by Allen.
 INSERT INTO t_category (name, create_user_id) VALUES ('java', 1);
